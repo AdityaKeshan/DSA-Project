12fada41ae1e1b12b5928ce8218f79cca594e26a
Sapphire2664 <aditya.keshan2019@vitstudent.ac.in>
2020-02-04 13:16:39 +0530

    Buttons declaration done

---
+++ MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1,button2,button3,button4,button5,button6,button7,button8,button9,buttonp,buttons,buttonc,buttonAC,buttonm,buttond,buttone;
+    EditText Value;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
